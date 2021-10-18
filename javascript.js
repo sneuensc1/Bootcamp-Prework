1bf41e471220ee9345f934f6cbf09c5bc4a43d80
sneuensc1 <91029412+sneuensc1@users.noreply.github.com>
2021-10-17 21:04:28 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,11 +1,24 @@
 (function() {
     //
-   document.getElementById("growButton").addEventListener("click", function(){
-       var box = document.getElementById('box')
-   })
+    
+    var box = document.getElementById('box');
+
+    var x = 150
+
+    x = x + 100
+
+    var y = x + "px"
+
+   
+    document.getElementById("growButton").addEventListener("click", function(){
+       box.style.height(y)
+   });
+   document.getElementById("growButton").addEventListener("click", fucntion(){
+       box.style.width(y)
+   });
    
    
    
    
   
-}
+})();
