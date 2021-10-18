1e3fd2794e2d49ad2598d91f2f5cc6980e2190df
sneuensc1 <91029412+sneuensc1@users.noreply.github.com>
2021-10-17 21:47:44 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -3,16 +3,14 @@
     
     var box = document.getElementById('box');
 
-    var x = 150
+    var x = "150px";
 
-    x = x + 100
-
-    var y = x + "px"
+    x= x + "100px";
 
    
     document.getElementById("growButton").addEventListener("click", function(){
-       box.style.height(y);
-       box.style.width(y);
+       box.style.height = x;
+       box.style.width = x;
    });
 
    
